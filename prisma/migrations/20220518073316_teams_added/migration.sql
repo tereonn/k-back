ac14a66d4d367dde5abedc5c2c411680b9b44a59
tereonn <tereonn@gmail.com>
2022-05-18 14:06:45 +0200

    Teams added to db

---
+++ prisma/migrations/20220518073316_teams_added/migration.sql
@@ -0,0 +1,13 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `teamId` INTEGER NULL;
+
+-- CreateTable
+CREATE TABLE `Team` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_teamId_fkey` FOREIGN KEY (`teamId`) REFERENCES `Team`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
